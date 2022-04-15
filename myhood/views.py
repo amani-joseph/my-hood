1f72c8ba612f5eed01c65b644869c95576b0927e
amani-joseph <mr.amanijoseph.com>
2022-04-15 13:11:07 +0300

    Created app and the requisite templates with routing, added bootstrap and landing page markup

---
+++ myhood/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    """
+    view to render the index.
+    """
+    title = 'My-Hood'
+    context = {
+        'title': title,
+
+    }
+    # return render(request, )
+    return render(request, 'myhood/index.html')
