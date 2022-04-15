de88c71342facc7d644b42ff77a84af8f8be2bc3
amani-joseph <mr.amanijoseph.com>
2022-04-16 01:47:20 +0300

    Added routing for Signup and login page

--- myhood/urls.py
+++ myhood/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('signup/', views.register, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='myhood/auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='myhood/auth/logut.html'), name='logout'),
     path('profile/', views.profile, name='users-profile'),
     path('edit_profile/<int:pk>', views.edit_profile, name='edit-profile'),
 ]
--- myhood/views.py
+++ myhood/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created {username} \n welcome to instagram')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, "myhood/auth/signup.html", {'form': form})
+
 def index(request):
     """
     view to render the index.
