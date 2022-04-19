93733eb76ad5c82bf13a5f7fb028aee6a175d743
amani-joseph <mr.amanijoseph.com>
2022-04-19 05:34:36 +0300

    Deployment attempt7 after adding buildpack

--- myhoodproject/settings.py
+++ myhoodproject/settings.py
@@ -22,8 +22,9 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+from os import environ
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 
 # adding config
 cloudinary.config(
